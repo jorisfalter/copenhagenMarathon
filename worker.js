fdc8b39318feaa66af8cadc8ee2efab951635a92
Joris Falter <joris@jorisfalter.com>
2024-12-05 15:32:45 +0100

    x

--- worker.js
+++ worker.js
@@ -15,12 +15,17 @@ export default {
       // Update the stored content in KV
       await env.copenhagenMarathon.put(PREVIOUS_PAGE_KEY, currentContent);
 
+      // // send push notificaiton
+      // await sendPushoverNotification(
+      //   env,
+      //   "https://secure.onreg.com/onreg2/bibexchange/?eventid=6591&language=us"
+      // ); // sending the link as the message
+
       // Send email notification about the change
-      await sendPushoverNotification(
+      await sendEmailNotification(
         env,
-        "https://secure.onreg.com/onreg2/bibexchange/?eventid=6591&language=us"
-      ); // sending the link as the message
-      await sendEmailNotification(env, currentContent);
+        "https://www.isa.nl/employment/vacancies/"
+      );
     } else {
       console.log("No changes detected.");
       //   await sendEmailNotification(env, "not changed");
@@ -32,7 +37,7 @@ export default {
 async function fetchWebsiteContent() {
   // for testing purposes, using my bridgeopen script
   const response = await fetch(
-    "https://secure.onreg.com/onreg2/bibexchange/?eventid=6591&language=us"
+    "https://www.isa.nl/employment/vacancies/"
     // "https://bridgeopen-0fd60d885493.herokuapp.com/ledstatus"
   );
 
@@ -41,21 +46,21 @@ async function fetchWebsiteContent() {
 }
 
 // Function to send a notification (using Pushover or other services)
-async function sendPushoverNotification(env, message) {
-  console.log("sending push");
-  const response = await fetch("https://api.pushover.net/1/messages.json", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({
-      token: env.PUSHOVER_TOKEN,
-      user: env.PUSHOVER_USER,
-      message: message,
-    }),
-  });
-  if (!response.ok) {
-    console.error("Failed to send push notification:", await response.text());
-  }
-}
+// async function sendPushoverNotification(env, message) {
+//   console.log("sending push");
+//   const response = await fetch("https://api.pushover.net/1/messages.json", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: new URLSearchParams({
+//       token: env.PUSHOVER_TOKEN,
+//       user: env.PUSHOVER_USER,
+//       message: message,
+//     }),
+//   });
+//   if (!response.ok) {
+//     console.error("Failed to send push notification:", await response.text());
+//   }
+// }
 
 // Function to send email notification
 async function sendEmailNotification(env, content) {
